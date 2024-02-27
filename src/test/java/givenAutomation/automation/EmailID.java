37de85113c3334241f1add7e3d210fd66403a2e8
sccpatel <151904357+sccpatel@users.noreply.github.com>
2024-02-27 15:26:08 +0530

    Full Naukari .com is UpDated

---
+++ src/test/java/givenAutomation/automation/EmailID.java
@@ -0,0 +1,10 @@
+package givenAutomation.automation;
+
+public class EmailID {
+
+	 String oldEmail = "patelvirendra08@gmail.com";
+	
+	 String newEmail = "er.virendra08@gmail.com";
+	
+	
+}
--- src/test/java/givenAutomation/automation/jenkinHello.java
+++ src/test/java/givenAutomation/automation/jenkinHello.java
@@ -12,32 +12,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-public class jenkinHello {
+public class jenkinHello extends EmailID {
 	@Test
-	public static void launck() throws InterruptedException, IOException {
+public void resumeUpDate() {
+	launck(oldEmail);
+	try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	launck(newEmail);
+	
+}
+	
+	
+	public static void launck(String email){
 
 		System.out.println("Hello Mr. Virendra");
 
-//		FileInputStream file=		   new FileInputStream("");
-//		Properties properti=	     new Properties();
-//		properti.load(file);
-//		properti.getProperty("password");
+
 		ChromeOptions co = new ChromeOptions();
 		co.addArguments("--remote-allow-origins=*");
 //		co.addArguments("--headless");
 		co.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
 		WebDriver driver = new ChromeDriver(co);
-		  
+
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 		driver.get("https://www.naukri.com/");
 		driver.findElement(By.xpath("//a[@id='login_Layer']")).click();
 //		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"))
-		.sendKeys("patelvirendra08@gmail.com");
-		driver.findElement(By.xpath("//input[@placeholder='Enter your password']"))
-		.sendKeys("8542");
+				.sendKeys(email);
+		driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("85");
 		driver.findElement(By.xpath("//button[text()='Login']")).click();
+
+		driver.findElement(By.xpath("(//a[normalize-space()='View profile'])")).click();
+
+		try {
+			driver.findElement(By.xpath("(//span[@class='edit icon'][normalize-space()='editOneTheme'])[2]")).click();
+		} catch (Exception e) {
+
+		}
+		try {
+			driver.findElement(By.xpath("(//span[@class='edit icon'][normalize-space()='editOneTheme'])[2]")).click();
+		} catch (Exception e) {
+
+		}
+
+		
+		driver.findElement(By.xpath("//button[@id='saveKeySkills']")).click();
+		driver.quit();
+		
 	}
 
 }
